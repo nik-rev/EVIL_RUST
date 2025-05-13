eef6ae2d942b3d92440babf4c07d866e1c78b4b3
Nik Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-05-13 17:39:41 +0100

    chore: remove trash files

--- errors.rs
+++
@@ -1,9 +0,0 @@
-#![no_main]
-pub fn missing_unsafe() {}
-unsafe fn missing_pub() {}
-pub unsafe fn arg_could_be_mut(arg: ()) {}
-pub unsafe fn references_are_banned(mut arg: &()) {}
-
-pub unsafe extern "C" fn main(mut argc: i32, mut argv: *mut *mut core::ffi::c_char) -> i32 {
-    0
-}
